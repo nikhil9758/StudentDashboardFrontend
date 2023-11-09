bc2ea50645661186bceacc0afd4aca972e5c8878
Nikhil Gupta <nikhil_gu@hcl.com>
2023-11-09 09:19:25 +0530

    update cors issue

--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const customFetch=axios.create({
-    baseURL:'https://cors-anywhere.herokuapp.com/https://student-dashboard-backend-mz3y.onrender.com/'
+    baseURL:'/'
 })
