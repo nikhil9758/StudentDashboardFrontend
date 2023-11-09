8278c4531385408b1b5e702d4914847dda0e1659
Nikhil Gupta <nikhil_gu@hcl.com>
2023-11-09 10:10:53 +0530

    update cors issue

--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -1,5 +1,18 @@
 import axios from "axios"
+// const { createProxyMiddleware } = require("http-proxy-middleware");
 
+
+
+// module.exports = function(app: any) {
+//     app.use(
+//         "/api",
+//         createProxyMiddleware({
+//             target: `https://student-dashboard-backend-mz3y.onrender.com/`,
+//             changeOrigin: true,
+//         })
+//     );
+// }
+    
 export const customFetch=axios.create({
-    baseURL:'/'
+    baseURL:'https://student-dashboard-backend-mz3y.onrender.com/'
 })
